fd4ec27363df09376345dff4d6346b1cd5cee0ab
Jesper Berth <jesper.berth@arrow.com>
2023-08-24 15:07:22 +0200

    Version 1

---
+++ README.md
@@ -0,0 +1,11 @@
+# pysecmail
+
+reads msg files from fortigate with failed SSL-VPN logins
+
+```bash
+
+docker build . -t pysecmail:latest
+
+docker run -it --mount type=bind,source="$(pwd)"/done,target=/usr/src/app/done --mount type=bind,source="$(pwd)"/messages,target=/usr/src/app/messages --mount type=bind,source="$(pwd)"/output,target=/usr/src/app/output pysecmail:latest
+
+```
--- main.py
+++ main.py
@@ -1,13 +1,61 @@
 import extract_msg
 import re
+import glob
+import shutil
+import csv
 
-f = r'test1.msg'  # Replace with yours
-msg = extract_msg.Message(f)
-msg_body = msg.body
+msgDir = "messages"
+doneDir = "done"
+outputFile = "output/iplist.txt"
+msgfiles = []
+iparray = []
+resultip = []
 
-reg = re.compile('[0-9]+(?:\.[0-9]{1,3}){3}')
+def readFiles():
+    for file in glob.glob(msgDir+"/*.msg"):
+        msgfiles.append(file)
 
-ip = re.findall( reg, msg_body )
+def writeOutput():
+    with open(outputFile, "w") as write_file:
+        for line in resultip:
+            write_file.write(" ".join(line) + "\n")
+    write_file.close()
 
-print(ip)
+def moveFile(f):
+    shutil.move(f, doneDir)
 
+def readExistingFile():
+    with open(outputFile, newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in data:
+            print(row)
+            iparray.append(row)
+    csvfile.close()
+
+def getIp(f):
+    msg = extract_msg.Message(f)
+    msg_body = msg.body
+    reg = re.compile('[0-9]+(?:\.[0-9]{1,3}){3}')
+    ip = re.findall( reg, msg_body )
+    iparray.append(ip)
+    moveFile(f)
+
+def remDuplicate():
+    [resultip.append(x) for x in iparray if x not in resultip]
+
+def main():
+    readFiles()
+
+    numberfiles = len(msgfiles)
+    str_numberfiles = str(numberfiles)
+    print("Reading " + str_numberfiles + " Msg files")
+
+    for file in msgfiles:
+        getIp(file)
+
+    readExistingFile()
+    remDuplicate()
+    writeOutput()
+
+if __name__ == "__main__":
+    main()
